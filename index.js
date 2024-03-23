50dd0f5f74720aca963a3eade19b6ae6da617144
NAEEMSA123 <164561340+NAEEMSA123@users.noreply.github.com>
2024-03-23 02:09:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+let bmi_calculator = await inquirer.prompt([
+    //user input
+    {
+        type: 'number',
+        name: "num1",
+        message: "Enter your first number"
+    },
+    //select operations
+    {
+        type: "list",
+        choices: ["addition", "subtraction", "multiplication", "Division", "Reminder"],
+        name: 'operator',
+        message: "select your operator for calculation"
+    },
+    //second number input
+    {
+        type: 'number',
+        name: "num2",
+        message: "Enter your second number"
+    }
+]);
+if (bmi_calculator.operator === "addition") {
+    console.log(bmi_calculator.num1 + bmi_calculator.num2);
+}
+else if (bmi_calculator.operator === "subtraction") {
+    console.log(bmi_calculator.num1 - bmi_calculator.num2);
+}
+else if (bmi_calculator.operator === "multiplication") {
+    console.log(bmi_calculator.num1 * bmi_calculator.num2);
+}
+else if (bmi_calculator.operator === "Division") {
+    console.log(bmi_calculator.num1 / bmi_calculator.num2);
+}
+else if (bmi_calculator.operator === "Reminder") {
+    console.log(bmi_calculator.num1 % bmi_calculator.num2);
+}
+else {
+    console.log("Invalid operators");
+}
